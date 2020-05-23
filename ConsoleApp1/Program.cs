98ee690cc914e494e939211e23119b253670a4a2
Hakan Kutluay <hkutluay@hotmail.com>
2020-05-23 15:16:55 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ZipContent.GoogleDrive;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Console.WriteLine("Hello World!");
+
+                Class1 o = new Class1();
+
+                o.Test().Wait();
+            }
+            catch (Exception exc)
+            {
+
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
---
+++ ZipContent.GoogleDrive/GoogleDrivePartialFileReader.cs
@@ -0,0 +1,33 @@
+﻿using Google.Apis.Drive.v3;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ZipContent.Core;
+
+namespace ZipContent.GoogleDrive
+{
+    public class GoogleDrivePartialFileReader : IPartialFileReader
+    {
+        private readonly string _fileId;
+        private readonly FilesResource.GetRequest _getRequest;
+
+        public GoogleDrivePartialFileReader(DriveService driveService, string fileId)
+        {
+            _fileId = fileId;
+            _getRequest = driveService.Files.Get(_fileId);
+            _getRequest.Fields = "size";
+        }
+
+        public async Task<long> ContentLength()
+        {
+            var response = await _getRequest.ExecuteAsync();
+            return response.Size.GetValueOrDefault();
+        }
+
+        public async Task<byte[]> GetBytes(ByteRange range)
+        {
+            var stream = new System.IO.MemoryStream();
+            await _getRequest.DownloadRangeAsync(stream, new RangeHeaderValue(range.Start, range.End));
+            return stream.ToArray();
+        }
+    }
+}
